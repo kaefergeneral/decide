6b5b5ff630508546e34f4f26626b4626a1af996a
kaefergeneral <kaefergeneral@posteo.de>
2020-07-15 17:01:26 +0200

    Adjusted for WAX and REX removed

--- contracts/decide/include/utility.hpp
+++ contracts/decide/include/utility.hpp
@@ -6,48 +6,46 @@
 
 #include <eosio/eosio.hpp>
 #include <eosio/asset.hpp>
-#include <eosio.system/eosio.system.hpp>
 
 using namespace std;
 using namespace eosio;
+using namespace decidespace;
 
-using user_resources = eosiosystem::user_resources;
-using del_bandwidth_table = eosiosystem::del_bandwidth_table;
-using rex_bal_table = eosiosystem::rex_balance_table;
+// using user_resources = eosiosystem::user_resources;
+// using del_bandwidth_table = eosiosystem::del_bandwidth_table;
 
 //eosio.token account table
+//code: eosio.token
+//scope: account.value
 struct eosio_account {
     asset balance;
-
     uint64_t primary_key() const { return balance.symbol.code().raw(); }
-
     EOSLIB_SERIALIZE(eosio_account, (balance))
 };
 typedef multi_index<name("accounts"), eosio_account> eosio_accounts_table;
 
-//defined in 
-asset get_staked_tlos(name owner) {
+//table containing staked system tokens
+//code: eosio
+//scope: from.value
+struct delegated_bandwidth {
+    name from;
+    name to;
+    asset net_weight;
+    asset cpu_weight;
+    // bool is_empty() const { return net_weight.amount == 0 && cpu_weight.amount == 0; }
+    uint64_t  primary_key() const { return to.value; }
+    EOSLIB_SERIALIZE(delegated_bandwidth, (from)(to)(net_weight)(cpu_weight))
+};
+typedef multi_index<name("delband"), delegated_bandwidth> del_bandwidth_table;
+
+//get staked blaance from delband table
+asset get_staked(name owner) {
     del_bandwidth_table delband(name("eosio"), owner.value);
     auto r = delband.find(owner.value);
-
     int64_t amount = 0;
-
     if (r != delband.end()) {
         amount = (r->cpu_weight.amount + r->net_weight.amount);
     }
-    
-    return asset(amount, symbol("TLOS", 4));
+    return asset(amount, decide::WAX_SYM);
 }
 
-asset get_tlos_in_rex(name owner) {
-    rex_bal_table rexbals(name("eosio"), name("eosio").value);
-    auto rb = rexbals.find(owner.value);
-
-    int64_t amount = 0;
-
-    if (rb != rexbals.end()) {
-        amount = rb->vote_stake.amount;
-    }
-
-    return asset(amount, symbol("TLOS", 4));
-}
