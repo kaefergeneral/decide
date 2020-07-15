2376de10985d36df3fa1821410975696d8868f59
kaefergeneral <kaefergeneral@posteo.de>
2020-07-15 16:51:29 +0200

    + added nonvoters and WAX accomodations
    + exchanged tlos references to wax
    + added nonvoters to decide to prevent certain accounts (e.g. exchanges wallets) from registering as voters

--- contracts/decide/src/decide.cpp
+++ contracts/decide/src/decide.cpp
@@ -3,10 +3,6 @@
 
 using namespace decidespace;
 
-decide::decide(name self, name code, datastream<const char*> ds) : contract(self, code, ds) {}
-
-decide::~decide() {}
-
 //======================== admin actions ========================
 
 ACTION decide::init(string app_version) {
@@ -21,15 +17,15 @@ ACTION decide::init(string app_version) {
     check(!configs.exists(), "contract already initialized");
 
     //initialize
-    string app_name = "Telos Decide";
+    string app_name = "Decide";
     map<name, asset> new_fees;
     map<name, uint32_t> new_times;
         
     //set default fees
-    new_fees[name("ballot")] = asset(100000, TLOS_SYM); //10 TLOS
-    new_fees[name("treasury")] = asset(5000000, TLOS_SYM); //500 TLOS
-    new_fees[name("archival")] = asset(10000, TLOS_SYM); //1 TLOS (per day)
-    new_fees[name("committee")] = asset(100000, TLOS_SYM); //100 TLOS
+    new_fees[name("ballot")] = asset(10'00000000, WAX_SYM); //10 WAX
+    new_fees[name("treasury")] = asset(500'00000000, WAX_SYM); //500 WAX
+    new_fees[name("archival")] = asset(1'00000000, WAX_SYM); //1 WAX (per day)
+    new_fees[name("committee")] = asset(100'00000000, WAX_SYM); //100 WAX
 
     //set default times
     new_times[name("minballength")] = uint32_t(60); //1 minute in seconds
@@ -40,7 +36,7 @@ ACTION decide::init(string app_version) {
     config new_config = {
         app_name, //app_name
         app_version, //app_version
-        asset(0, TLOS_SYM), //total_deposits
+        asset(0, WAX_SYM), //total_deposits
         new_fees, //fees
         new_times //times
     };
@@ -77,8 +73,8 @@ ACTION decide::updatefee(name fee_name, asset fee_amount) {
     auto conf = configs.get();
     
     //validate
-    check(fee_amount.symbol == TLOS_SYM,  "fee symbol must be TLOS");
-    check(fee_amount >= asset(0, TLOS_SYM), "fee amount must be a positive number");
+    check(fee_amount.symbol == WAX_SYM,  "fee symbol must be WAX");
+    check(fee_amount >= asset(0, WAX_SYM), "fee amount must be a positive number");
 
     //initialize
     config new_conf = conf;
@@ -114,22 +110,49 @@ ACTION decide::updatetime(name time_name, uint32_t length) {
 
 }
 
+ACTION decide::updatenvoter(name nonvoter, string comment, bool remove){
+        
+    //authenticate
+    require_auth(get_self());
+    check(is_account(nonvoter), "Account doesn't exist.");
+
+    //open nonvoters table, look up account
+    nonvoters_table nonvoters(get_self(), get_self().value);
+    auto nvtr = nonvoters.find(nonvoter.value);
+
+    //if new entry
+    if (nvtr == nonvoters.end()) {
+        check(!comment.empty(), "Comment required.");
+        nonvoters.emplace(get_self(), [&]( auto& row ) {
+            row.nvoter = nonvoter;
+            row.comment = comment;
+        });
+    } else {
+         if (remove) {
+            nonvoters.erase(nvtr);
+        } else {
+            nonvoters.modify(nvtr, get_self(), [&](auto& row) {
+            row.comment = comment;});
+        }
+    }
+}
+
 ACTION decide::withdraw(name voter, asset quantity) {
     
     //authenticate
     require_auth(voter);
     
     //open accounts table, get account
-    accounts_table tlos_accounts(get_self(), voter.value);
-    auto& acct = tlos_accounts.get(TLOS_SYM.code().raw(), "account not found");
+    accounts_table wax_accounts(get_self(), voter.value);
+    auto& acct = wax_accounts.get(WAX_SYM.code().raw(), "account not found");
 
     //validate
-    check(quantity.symbol == TLOS_SYM, "can only withdraw TLOS");
+    check(quantity.symbol == WAX_SYM, "can only withdraw WAX");
     check(acct.balance >= quantity, "insufficient balance");
-    check(quantity > asset(0, TLOS_SYM), "must withdraw a positive amount");
+    check(quantity > asset(0, WAX_SYM), "must withdraw a positive amount");
 
     //update balances
-    tlos_accounts.modify(acct, same_payer, [&](auto& col) {
+    wax_accounts.modify(acct, same_payer, [&](auto& col) {
         col.balance -= quantity;
     });
 
@@ -141,20 +164,21 @@ ACTION decide::withdraw(name voter, asset quantity) {
     configs.set(config, get_self());
 
     //transfer to eosio.token
-    //inline trx requires telos.decide@active to have telos.decide@eosio.code
+    //inline trx requires wax.decide@active to have wax.decide@eosio.code
     //TODO: replace with action handler
     // token::transfer_action transfer_act("eosio.token"_n, { get_self(), active_permission });
-    // transfer_act.send(get_self(), voter, quantity, std::string("Telos Decide Withdrawal"));
+    // transfer_act.send(get_self(), voter, quantity, std::string("Wax Decide Withdrawal"));
 
     //inline transfer
     action(permission_level{get_self(), name("active")}, name("eosio.token"), name("transfer"), make_tuple(
         get_self(), //from
         voter, //to
         quantity, //quantity
-        std::string("Telos Decide Withdrawal") //memo
+        std::string("Wax Decide Withdrawal") //memo
     )).send();
 
 }
+
 
 //========== notification methods ==========
 
@@ -173,7 +197,7 @@ void decide::catch_transfer(name from, name to, asset quantity, string memo) {
     name rec = get_first_receiver();
 
     //validate
-    if (rec == token_account && from != get_self() && quantity.symbol == TLOS_SYM) {
+    if (rec == name("eosio.token") && from != get_self() && quantity.symbol == WAX_SYM) {
         
         //parse memo
         //NOTE: keyword should maybe be "refund" or "return"
@@ -182,7 +206,7 @@ void decide::catch_transfer(name from, name to, asset quantity, string memo) {
         
         //open accounts table, search for account
         accounts_table accounts(get_self(), from.value);
-        auto acct = accounts.find(TLOS_SYM.code().raw());
+        auto acct = accounts.find(WAX_SYM.code().raw());
 
         //emplace account if not found, update if exists
         if (acct == accounts.end()) { //no account
@@ -201,16 +225,16 @@ void decide::catch_transfer(name from, name to, asset quantity, string memo) {
         config.total_deposits += quantity;
 
         configs.set(config, get_self());
-    } else if (rec == token_account && from == get_self() && quantity.symbol == TLOS_SYM) {
+    } else if (rec == name("eosio.token") && from == get_self() && quantity.symbol == WAX_SYM) {
         config_singleton configs(get_self(), get_self().value);
         auto config = configs.get();
 
         eosio_accounts_table eosio_accounts(name("eosio.token"), get_self().value);
-        auto& eosio_acct = eosio_accounts.get(TLOS_SYM.code().raw(), "tlos balance not found");
+        auto& eosio_acct = eosio_accounts.get(WAX_SYM.code().raw(), "WAX balance not found");
 
         asset total_transferable = (eosio_acct.balance + quantity) - config.total_deposits;
         
-        check(total_transferable >= quantity, "Telos Decide lacks the liquid TLOS to make this transfer");
+        check(total_transferable >= quantity, "Decide lacks the liquid WAX to make this transfer");
     }
 }
 
@@ -322,12 +346,12 @@ bool decide::valid_access_method(name access_method) {
 }
 
 void decide::require_fee(name account_name, asset fee) {
-    //open accounts table, get TLOS balance
-    accounts_table tlos_accounts(get_self(), account_name.value);
-    auto& tlos_acct = tlos_accounts.get(TLOS_SYM.code().raw(), "TLOS balance not found");
+    //open accounts table, get WAX balance
+    accounts_table wax_accounts(get_self(), account_name.value);
+    auto& wax_acct = wax_accounts.get(WAX_SYM.code().raw(), "WAX balance not found");
 
     //validate
-    check(tlos_acct.balance >= fee, "insufficient funds to cover fee");
+    check(wax_acct.balance >= fee, "insufficient funds to cover fee");
 
     config_singleton configs(get_self(), get_self().value);
     auto config = configs.get();
@@ -337,7 +361,7 @@ void decide::require_fee(name account_name, asset fee) {
     configs.set(config, get_self());
 
     //charge fee
-    tlos_accounts.modify(tlos_acct, same_payer, [&](auto& col) {
+    wax_accounts.modify(wax_acct, same_payer, [&](auto& col) {
         col.balance -= fee;
     });
 }
@@ -345,15 +369,14 @@ void decide::require_fee(name account_name, asset fee) {
 void decide::sync_external_account(name voter, symbol internal_symbol, symbol external_symbol) {
     
     //initialize
-    asset tlos_stake;
+    asset wax_stake;
 
-    //check external symbol is TLOS, if not then return
-    if (external_symbol == TLOS_SYM) {
-        tlos_stake = get_staked_tlos(voter);
-        tlos_stake += get_tlos_in_rex(voter);
-        check(internal_symbol == VOTE_SYM, "internal symbol must be VOTE for external TLOS");
+    //check external symbol is WAX, if not then return
+    if (external_symbol == WAX_SYM) {
+        wax_stake = get_staked(voter);
+        check(internal_symbol == VOTE_SYM, "internal symbol must be VOTE for external WAX");
     } else {
-        check(false, "syncing other external accounts is under development");
+        check(false, "syncing other external accounts is disabled");
         return;
     }
 
@@ -373,16 +396,16 @@ void decide::sync_external_account(name voter, symbol internal_symbol, symbol ex
             return;
             
         //calc delta
-        asset delta = asset(tlos_stake.amount - vtr_itr->staked.amount, internal_symbol);
+        asset delta = asset(wax_stake.amount - vtr_itr->staked.amount, internal_symbol);
 
         //apply delta to supply
         treasuries.modify(*trs_itr, same_payer, [&](auto& col) {
             col.supply += asset(delta.amount, internal_symbol);
         });
 
-        //mirror tlos_stake to internal_symbol stake
+        //mirror wax_stake to internal_symbol stake
         voters.modify(*vtr_itr, same_payer, [&](auto& col) {
-            col.staked = asset(tlos_stake.amount, internal_symbol);
+            col.staked = asset(wax_stake.amount, internal_symbol);
         });
 
     }
